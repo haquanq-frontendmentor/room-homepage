e1e54494d4556730a8351ec4cebd3b075347b776
Ha Quanq <quangha0120010@gmail.com>
2025-10-08 23:50:59 +0700

    Add base path for build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [tailwindcss(), solid()],
+    base: "/room-homepage",
 });
